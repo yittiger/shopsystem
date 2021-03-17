498bbd76a87f3a3287f472d8cc30f9befcc49776
yittiger <2687033794@qq.com>
2021-03-18 04:44:58 +0800

    home页布局以及推出功能制作

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,13 +1,48 @@
 <template>
-  <div></div>
+  <div class="home-wrap">
+    <el-container>
+      <el-header>
+        <img class="logoImg" src="~assets/img/logo.png">
+        <el-button @click="logout">退出</el-button>
+      </el-header>
+      <el-container>
+        <el-aside width="220px">Aside</el-aside>
+        <el-main>Main</el-main>
+      </el-container>
+    </el-container>
+  </div>
 </template>
 
 <script>
   export default {
-    name: "Home"
+    name: "Home",
+    methods: {
+      logout() {
+        window.sessionStorage.clear()
+        this.$router.replace('/login')
+      }
+    }
   }
 </script>
 
 <style scoped>
-
+.home-wrap, .el-container{
+  width: 100%;
+  height: 100%;
+}
+  .el-header{
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    background-color: #373f41;
+  }
+  .logoImg{
+    height: 50px;
+  }
+  .el-aside{
+    background-color: #333744;
+  }
+  .el-main{
+    background-color: #eaedf1;
+  }
 </style>
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,9 +1,13 @@
 import Vue from 'vue'
-import {Form, FormItem, Input, Button, Message} from 'element-ui'
+import {Form, FormItem, Input, Button, Message, Container, Header, Aside, Main} from 'element-ui'
 
 Vue.use(Form)
   .use(FormItem)
   .use(Input)
   .use(Button)
+  .use(Container)
+  .use(Header)
+  .use(Aside)
+  .use(Main)
 
 Vue.prototype.$message = Message
